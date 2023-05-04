ac6d59037f69aa9e3b1f12b9684f00007a37e926
ivan <ivanoblonsky@gmail.com>
2023-05-04 11:46:14 +0300

    Create superuser 'zahar'

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email")
+
 # Register your models here.
+admin.site.register(User)
